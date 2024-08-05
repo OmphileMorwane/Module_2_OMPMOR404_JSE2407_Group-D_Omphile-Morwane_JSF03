c6b8872dccc0e0c9bb37a517d5f22d5193aecbb5
Omphile Morwane <omphilemorwane@gmail.com>
2024-08-05 08:45:15 +0200

    Updated the App file by importing comp files

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <Header />
+    <ProductList />
+  </div>
+</template>
+
+<script setup>
+import Header from './components/Header.vue'
+import ProductList from './components/ProductList.vue'
+</script>
+
+<style>
+html, body {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden; 
+  background-color: darkcyan;
+}
+
+#app {
+  margin:0;
+ padding: 0;
+  width: 100%;
+}
+
+* {
+  box-sizing:content-box; /* Include padding and border in element's total width and height */
+}
+
+</style>
+
