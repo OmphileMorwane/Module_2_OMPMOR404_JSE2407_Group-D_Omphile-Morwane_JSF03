2bc300b60f77a0c5f320deb6f928607d2e577624
Omphile Morwane <omphilemorwane@gmail.com>
2024-08-06 11:17:27 +0200

    corrected syntax

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Header />
+    <Header></Header>
      <router-view></router-view>
     
    
@@ -8,19 +8,23 @@
 </template>
 
 <script setup>
+/**
+ * Import the Header component.
+ */
 import Header from './components/Header.vue'
 
 </script>
 
-<style>
+<style >
 html, body {
   margin: 0;
   padding: 0;
   width: 100%;
   height: 100%;
   overflow-x: hidden; 
-  background-color: #9cdfe8;
+  background-color: #e1f7fa;
   color: #333333;
+
   
 }
 
