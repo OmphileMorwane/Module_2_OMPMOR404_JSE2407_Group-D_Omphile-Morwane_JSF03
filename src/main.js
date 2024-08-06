c2f7878d5e5c6835e9788d0bea7683b9cedd98bd
Omphile Morwane <omphilemorwane@gmail.com>
2024-08-06 11:14:47 +0200

    Added documentation

--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ import './style.css'
 import App from './App.vue'
 import router from './components/router'
 
+/**
+ * Creates and mounts the Vue application.
+ */
 const app = createApp(App)
+
+/**
+ * Uses the Vue Router for the application.
+ */
 app.use(router);
+
+
+/**
+ * Mounts the Vue application to the DOM element with the id 'app'.
+ */
 app.mount('#app');
